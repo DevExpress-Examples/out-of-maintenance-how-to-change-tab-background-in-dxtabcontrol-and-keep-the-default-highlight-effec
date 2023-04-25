8fb7f7c8f4511784a1e9479485caccee4bd7f068
AndreySlabov <andrey.slabov@devexpress.com>
2023-04-25 17:50:20 +0400

    BackgroundColor -> AccentColor

--- CS/DXTabControlExample/MainWindow.xaml.cs
+++ CS/DXTabControlExample/MainWindow.xaml.cs
@@ -1,30 +1,9 @@
-﻿using DevExpress.Xpf.Core;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace DXTabControlExample
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : DXWindow
-    {
-        public MainWindow()
-        {
+namespace DXTabControlExample {
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
         }
-
     }
 }
