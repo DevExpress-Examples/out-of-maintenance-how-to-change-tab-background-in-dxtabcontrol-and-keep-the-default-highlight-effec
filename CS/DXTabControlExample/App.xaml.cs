0631fe69221dab1d564e5b00422645d9a5d81f8e
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-12-21 14:26:35 +0400

    Change example status to out of maintenance

--- CS/DXTabControlExample/App.xaml.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Windows;
-
-namespace DXTabControlExample {
-    public partial class App : Application {
-    }
-}
--- CS/DXTabControlExample/MainWindow.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows;
-
-namespace DXTabControlExample {
-    public partial class MainWindow : Window {
-        public MainWindow() {
-            InitializeComponent();
-        }
-    }
-}
